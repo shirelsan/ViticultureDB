79887568633fad0d3ca4f1beb1565fe5fd4d8619
tallon2023 <154704843+tallon2023@users.noreply.github.com>
2025-05-05 17:29:59 +0300

    Update UpdateQueriesnew.sql

--- Phrase2/RollbackCommit.sql/UpdateQueriesnew.sql
+++ Phrase2/RollbackCommit.sql/UpdateQueriesnew.sql
@@ -20,3 +20,17 @@ WHERE vine_id IN (
         EXTRACT(MONTH FROM h.harvest_date) = 1 AND
         h.quantity > 65
 );
+
+
+-- Update Query 3: Increase inventory quantity by 10% where harvested quantity is greater than inventory quantity, but only for red grapes
+UPDATE inventory
+SET inventory_quantity = inventory_quantity * 1.10
+WHERE inventory_id IN (
+    SELECT h.inventory_id
+    FROM harvest h
+    JOIN vineyard v ON h.harvest_id = v.harvest_id
+    JOIN grape_variety g ON v.vine_id = g.vine_id
+    JOIN inventory i ON h.inventory_id = i.inventory_id
+    WHERE g.color = 'Red'
+      AND h.quantity > i.inventory_quantity
+);
