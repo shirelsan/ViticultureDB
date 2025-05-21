fb51968e36d4939d7bbaecb5fef4af421aa68dc5
tallon2023 <154704843+tallon2023@users.noreply.github.com>
2025-05-21 19:12:16 +0300

    Add files via upload

---
+++ Phrase4/files.sql/triger1.sql
@@ -0,0 +1,43 @@
+CREATE OR REPLACE FUNCTION update_equipment_status()
+RETURNS TRIGGER AS $$
+DECLARE
+    usage_count INT;
+BEGIN
+    SELECT COUNT(*) INTO usage_count
+    FROM Process_Equipment
+    WHERE EquipmentID_ = NEW.EquipmentID_;
+
+    IF usage_count >= 3 THEN
+        UPDATE ProductionEquipment_
+        SET status_ = 'Overloaded'
+        WHERE EquipmentID_ = NEW.EquipmentID_;
+    ELSE
+        UPDATE ProductionEquipment_
+        SET status_ = 'InUse'
+        WHERE EquipmentID_ = NEW.EquipmentID_;
+    END IF;
+
+    RETURN NEW;
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE NOTICE 'שגיאה בעדכון סטטוס לציוד %: %', NEW.EquipmentID_, SQLERRM;
+        RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trg_update_equipment_status ON Process_Equipment;
+
+CREATE TRIGGER trg_update_equipment_status
+AFTER INSERT ON Process_Equipment
+FOR EACH ROW
+EXECUTE FUNCTION update_equipment_status();
+
+
+--דוגמא של הכנסת ערכים בשביל להראות את הטריגר
+INSERT INTO ProductionEquipment_ (EquipmentID_, Type, Status)
+VALUES (100, 'Home', 'Available');
+
+INSERT INTO Process_Equipment (EquipmentID_, ProcessID_) VALUES (100, 101);
+INSERT INTO Process_Equipment (EquipmentID_, ProcessID_) VALUES (100, 100);
+INSERT INTO Process_Equipment (EquipmentID_, ProcessID_) VALUES (100, 397);
+INSERT INTO Process_Equipment (EquipmentID_, ProcessID_) VALUES (100, 398);
